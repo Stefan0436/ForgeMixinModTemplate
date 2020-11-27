a70b7f403013e4db33e88b933946d5f61e8544dc
Stefan Eekhof <52248836+Stefan0436@users.noreply.github.com>
2020-11-27 10:45:00 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read -rp "Git url: " url; read -rp "Output folder name: " outputname
+read -rp "Git url: " url
+read -rp "Output folder name: " outputname
 git clone $url "$outputname"
 cd "$outputname"
 chmod ug=rwx gradlew
