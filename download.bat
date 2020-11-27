20bb4ca5d7efb80b65fbe800b9220e8b64238d60
Stefan Eekhof <52248836+Stefan0436@users.noreply.github.com>
2020-11-27 10:53:00 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p url="Git url: "
-set /p outputname="Output folder name: "
+set /p url=Git url: 
+set /p outputname=Output folder name: 
 git clone %url% "%outputname%"
 cd %outputname% 
